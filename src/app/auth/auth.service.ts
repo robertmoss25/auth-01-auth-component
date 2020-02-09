119ddba004a2430b6c92a142e860a81e349271a1
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-02-09 13:14:07 -0500

    Adding Auto Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -48,6 +48,23 @@ export class AuthService {
         );
     }
 
+    autologin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -57,6 +74,7 @@ export class AuthService {
         const expirationdate = new Date(new Date().getTime() + +expiresIn * 1000);
         const user = new User(email, userId, token, expirationdate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
