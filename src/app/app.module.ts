8ce465d0e3ee5a99b636dbeabd9e688b79084209
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-02-09 14:56:54 -0500

    Understanding entryComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,6 +49,9 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
     AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService,{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule {}
